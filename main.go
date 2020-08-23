a47dd3306fc182ebc522c09e3975dd369a550644
Rohit Rohit <rohit.rohit@anz.com>
2020-08-23 16:32:26 +1000

    Adding custom gitaction

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
-import "github.com/rohitjb/heapProblems/maxheap"
-
 func main() {
-	items := []int{1, 23, 12, 9, 30, 2, 50}
-	k := 6
-	maxheap.KthSmallestElements(items, k)
+	//items := []int{1, 23, 12, 9, 30, 2, 50}
+	//k := 6
+	//maxheap.KthSmallestElements(items, k)
 }
