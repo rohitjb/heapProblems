615faa737a282b35841a4471c59c0fd4c13263d8
Rohit Rohit <rohit.rohit@anz.com>
2020-08-25 10:51:48 +1000

    adding container test

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,6 +1,10 @@
-#!/bin/sh -l
+#!/bin/sh
 
-#echo "Hello $1"
-#time=$(date)
-#echo "::set-output name=time::$time"
-conftest --version
+echo "Downloading latest container-structure-test binary..."
+
+wget -O container-structure-test -nv "${CONTAINER_STRUCTURE_TEST_DOWNLOAD_URL}" \
+  && mv container-structure-test /usr/local/bin \
+  && chmod +x /usr/local/bin/container-structure-test \
+  && export PATH=$PATH:/usr/local/bin
+
+container-structure-test $@
