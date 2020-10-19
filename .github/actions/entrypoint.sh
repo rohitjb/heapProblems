0393d00da859d6ce6725064c5fa58ebbc6a42417
Rohit Rohit <rohit.rohit@anz.com>
2020-10-19 23:01:46 +1100

    fsdf

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+#BASE_URL='https://blackduck.platform-services.services-platdev.x.gcpnp.anz'
+#AUTHENTICATE='api/tokens/authenticate'
+#
+#TOKEN="token $1"
+#COMPONENT_JSON_URL="$2"
+#
+#token=$(curl --request POST --url ${BASE_URL}/${AUTHENTICATE} --header "Authorization: $TOKEN" | jq -r '.bearerToken')
+#curl --request GET --url ${COMPONENT_JSON_URL} --header "Authorization: Bearer $token" | tee report.json
+
 BASE_URL='https://blackduck.platform-services.services-platdev.x.gcpnp.anz'
 AUTHENTICATE='api/tokens/authenticate'
 
 TOKEN="token $1"
-COMPONENT_JSON_URL="$2"
 
-token=$(curl --request POST --url ${BASE_URL}/${AUTHENTICATE} --header "Authorization: $TOKEN" | jq -r '.bearerToken')
-curl --request GET --url ${COMPONENT_JSON_URL} --header "Authorization: Bearer $token" | tee report.json
-
+curl --request POST --url ${BASE_URL}/${AUTHENTICATE} --header "Authorization: $TOKEN" | jq -r '.bearerToken'
