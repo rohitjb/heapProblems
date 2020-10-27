31550f642ffdf8fa1852f2d2269c9fd65f07413c
Rohit Rohit <rohit.rohit@anz.com>
2020-10-27 16:26:45 +1100

    adding

--- .github/actions/blackduck-scan-action/blackduck.sh
+++ .github/actions/blackduck-scan-action/blackduck.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "Goodbye"
-echo Hello $1
+
+BASE_URL='https://blackduck.platform-services.services-platdev.x.gcpnp.anz'
+AUTHENTICATE='api/tokens/authenticate'
+bearerToken=$(curl --request POST --url ${BASE_URL}/${AUTHENTICATE} --header "Authorization: token $1" | jq -r '.bearerToken')
+
+echo $bearerToken
